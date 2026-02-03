8da71e19f7aaa506985f3c9d716e7f86d2ec1669
Yosef Janan <143737916+Necrolad@users.noreply.github.com>
2026-02-03 08:54:35 -0500

    bugfix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const btPlus = document.querySelector('.js-plus');
 const conteneur = document.querySelector('.conteneur-transports');
 
-btPlus.addEventListener('click, fouvreferme');
+btPlus.addEventListener('click', fouvreferme);
 
 function fouvreferme(){
 
-    conteneur.classlist.toggle('conteneur-transports-invisible');    
+    conteneur.classList.toggle('conteneur-transports-invisible');    
 }
 
 
