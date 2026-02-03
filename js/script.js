e1b4d87c2b44fcfbdb6d22d1297c084b5621b043
Yosef Janan <143737916+Necrolad@users.noreply.github.com>
2026-02-03 08:34:33 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 const btPlus = document.querySelector('.js-plus');
 const conteneur = document.querySelector('.conteneur-transports');
 
+btPlus.addEventListener('click, fouvreferme');
 
+function fouvreferme(
+
+    conteneur.classlist.toggle('conteneur-transports-invisible');
+)
 
 
 
